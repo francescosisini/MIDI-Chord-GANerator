88ed0bc8170ec43494ece6d46a2d9749fe99bb25
francescosisini <francescomichelesisini@gmail.com>
2024-07-19 17:39:54 +0200

    Aggiunta funzionalità per la generazione di melodie casuali, gestione del volume delle tracce e aggiornamento del README

--- melody.h
+++ melody.h
@@ -3,8 +3,8 @@
 
 #include <smf.h>
 
-void generate_random_notes(smf_track_t *track, int num_notes, double start_time, double duration);
-void generate_random_notes_with_scale(smf_track_t *track, int num_notes, double start_time, double duration, int* scale_notes, int num_scale_notes);
+void generate_random_notes(smf_track_t* track, int num_notes, double start_time, double duration, int volume);
+void generate_random_notes_with_scale(smf_track_t* track, int num_notes, double start_time, double duration, int* scale_notes, int num_scale_notes, int volume);
 void get_scale_notes(const char* key, int* scale_notes, int* num_notes);
 
-#endif // MELODY_H
+#endif
